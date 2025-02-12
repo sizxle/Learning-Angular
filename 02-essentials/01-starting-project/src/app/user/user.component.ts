eac4e5309115e91a0b3d2e18e4ba530453e8f262
sthembizo.molefi <sthembizo@psybergate.co.za>
2025-02-12 13:39:10 +0200

    init commit

---
+++ 02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {User} from "./user.model"
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required:true}) user !: User;
+  @Output() select = new EventEmitter();
+
+  get imagePath(){
+    return 'assets/users/'+this.user.avatar
+  }
+
+  onSelectUser(){
+    this.select.emit(this.user.id)
+  }
+}
