49f2616b141d789e75ff9ad2f1c216bf0ec25cd3
sthembizo.molefi <sthembizo@psybergate.co.za>
2025-02-13 20:25:33 +0200

    use local storage

--- 02-essentials/01-starting-project/src/app/tasks/tasks.service.ts
+++ 02-essentials/01-starting-project/src/app/tasks/tasks.service.ts
@@ -29,6 +29,13 @@ export class TaskService{
       },
     ]
 
+    constructor(){
+      const tasks= localStorage.getItem('tasks');
+      if(tasks){
+        this.tasks=JSON.parse(tasks);
+      }
+    }
+
     public getUserTasks(userId:string){
       return this.tasks.filter(task => task.userId==userId)
     }
@@ -43,9 +50,15 @@ export class TaskService{
           dueDate:taskData.date
         }
       )
+      this.saveTasks()
     }
 
     public removeTask(taskId:string){
       this.tasks= this.tasks.filter(task=>task.id!=taskId)
+      this.saveTasks()
+    }
+
+    private saveTasks(){
+      localStorage.setItem('tasks',JSON.stringify(this.tasks));
     }
 }
